580257e49900ae9a771b4a0255164e91efa45425
Alexander <alykoshin@users.noreply.github.com>
2023-09-05 00:18:34 +0300

    add dist/*

--- dist/recipes/yarnInstallProd.d.ts
+++ dist/recipes/yarnInstallProd.d.ts
@@ -1,6 +1,15 @@
 import { FullConfig } from "../lib/config";
 import { Runner } from "../lib/runner";
 import { ExecAction } from "./exec";
+export interface YarnInstallProdAction {
+    action: 'yarnInstallProd';
+    config: {
+        cwd: string;
+        env: {
+            [key: string]: string;
+        };
+    };
+}
 export declare function action_yarnInstallProd(definition: ExecAction, { id, fullConfig, runner }: {
     id: number | string;
     fullConfig: FullConfig;
