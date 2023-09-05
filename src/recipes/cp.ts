9757ee88720d022ad4544c9375430fd5bda721f4
Alexander <alykoshin@users.noreply.github.com>
2023-09-05 14:31:04 +0300

    recipes cp/rm

--- src/recipes/cp.ts
+++ src/recipes/cp.ts
@@ -6,18 +6,19 @@ export interface CpAction {
   action: 'cp'
   source: string | string[]
   dest: string
-  dry: boolean
+  dry?: boolean
 }
 
 export async function action_cp(
   definition: CpAction,
   {id, fullConfig, runner}: { id: number | string, fullConfig: FullConfig, runner: Runner }
 ) {
+  const dry = typeof definition.dry !== 'undefined' ? definition.dry : false;
   const sources = Array.isArray(definition.source) ? definition.source : [definition.source];
   for (let src of sources) {
     runner.debug(id, `[cp] copying "${src}"`)
     if (definition.dry) {
-      runner.debug(id, `[rm] "dry" flag is set; skipping`);
+      runner.debug(id, `[cp] "dry" flag is set; skipping`);
     } else {
       await fs.cp(src, definition.dest);
     }
--- src/recipes/rm.ts
+++ src/recipes/rm.ts
@@ -5,21 +5,23 @@ import {Runner} from "../lib/runner";
 export interface RmAction {
   action: 'rm'
   files: string|string[]
-  dry: boolean
+  dry?: boolean
 }
 
 export async function action_rm(
   definition: RmAction,
   {id, fullConfig, runner}: { id: number | string, fullConfig: FullConfig, runner: Runner }
 ) {
+  const dry = typeof definition.dry !== 'undefined' ? definition.dry : false;
   const files = Array.isArray(definition.files) ? definition.files : [definition.files];
   for (let pathname of files) {
     runner.debug(id, `[rm] deleting file "${pathname}"`)
-    if (definition.dry) {
+    if (dry) {
       runner.debug(id, `[rm] "dry" flag is set; skipping`);
     } else {
       await fs.rm(pathname);
     }
   }
+  runner.debug(id, `[cp] deleted ${files.length} dirs/files`)
 }
 
