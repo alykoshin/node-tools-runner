0a029c5953e576cf05705c28b0fcc9e16a09fac9
Alexander <alykoshin@users.noreply.github.com>
2023-09-30 02:07:33 +0300

    v0.0.19

---
+++ src/tests/$zip.ts
@@ -0,0 +1,22 @@
+import {Activity} from "..\\lib\\config";
+
+export const config: Activity = {
+  base_dir: '.',
+  version: '0.0.0',
+  actions: {
+    default: [
+      'list', [
+        '$zip', {
+          file_names: [ '.\\src\\tests\\data\\in\\', ],
+          archive_prefix: 'dist',
+          out_dir: '.\\src\\tests\\data\\out\\',
+          exclude_files: [
+            'test1.txt',
+          ],
+        }
+      ],
+    ],
+  },
+}
+
+export default config;
